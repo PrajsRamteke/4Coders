ef6efedd38985931ca5dfa50863e5065bd486046
Prajwal <prajwalramteke9@gmail.com>
2023-04-12 23:14:47 +0530

    increase token

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ btn.addEventListener("click", async (event) => {
         prompt: prompt,
         model: "text-davinci-003",
         temperature: 0,
-        max_tokens: 1000,
+        max_tokens: 3000,
         top_p: 1,
         frequency_penalty: 0.0,
         presence_penalty: 0.0,
